1bf92c818d9f505c81a8e39a2a03fbc94ec55178
juxsalley <juxsalley@gmail.com>
2021-04-26 11:41:11 +0000

    generated models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :students, only: [:index, :show]
+
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210426113712_create_students.rb
@@ -0,0 +1,16 @@
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+      t.string :index_number
+      t.string :password_digest
+      t.string :address
+      t.string :course
+      t.date :enroll_year
+
+      t.timestamps
+    end
+  end
+end
