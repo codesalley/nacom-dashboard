ff07e565eddb8b94bf07078b63442d0f3c460929
juxsalley <juxsalley@gmail.com>
2021-04-29 15:09:41 +0000

    recreate db

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,6 +1,6 @@
 class Announcement < ApplicationRecord
     validates :title, presence: true
-    # has_rich_text :content
+
 
     belongs_to :department, class_name: "Department", foreign_key: "department_id"
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,4 @@ class Message < ApplicationRecord
 
     belongs_to :student, class_name: "Student", foreign_key: "student_id"
 
-    # has_rich_text :content
 end
